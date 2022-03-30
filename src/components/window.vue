f1dc2cee6749353650d5623e79741933f9318b02
Thiago099 <tecnologiasthiago@gmail.com>
2022-03-30 19:49:20 -0300

    [fix]

--- src/components/window.vue
+++ src/components/window.vue
@@ -1,9 +1,14 @@
 <template>
-    <div ref="window" class="window" :class="{'maximized':maximized}"  @mousedown="$emit('focus')">
+    <div 
+      ref="window" 
+      class="window" 
+      :style="maximized?'border-radius:0px':''"  
+      @mousedown="$emit('focus')"
+    >
       <div ref='header' class="header" :drag="true">
         <div class="title"><img class = "icon" ref="icon" :src="require('@/assets/' + icon + '')">{{ title }}</div>
         <div @mousedown="preventDrag">
-          <div class="nav-button close-button" @click="$emit('close')"><div>×</div></div>
+          <div class="nav-button close-button" :style="maximized?'border-radius:0px':''"  @click="$emit('close')"><div>×</div></div>
           <div class="nav-button maximize-button" v-if="maximized" @click="handleMinimize()"><div>🗗</div></div>
           <div class="nav-button minimize-button" v-else @click="handleMaximize()"><div>□</div></div>
           <div class="nav-button minus-button" @click="$emit('minus')"><div>-</div></div>
@@ -15,8 +20,8 @@
     </div>
 </template>
 <script>
-import {dragElement, set_draggable} from './plugins/drag.js'
-import {resizeElement, set_resizable} from './plugins/resize.js'
+
+
   export default {
     props: {
       title: {
@@ -29,7 +34,33 @@ import {resizeElement, set_resizable} from './plugins/resize.js'
     },
     data(){
       return {
+        draggable: false,
+        drag_handle: null,
+        pos1: 0,
+        pos2: 0,
+        pos3: 0,
+        pos4: 0,
+        resizable: true,
+        top: null,
+        left: null,
+        right: null,
+        bottom: null,
+        corner1: null,
+        corner2: null,
+        corner3: null,
+        corner4: null,
+        offsetX: null,
+        offsetY: null,
+        startX: null,
+        startW: null,
+        maxX: null,
+        minW: 100,
+        minH:100,
+        startY: null,
+        startH: null,
+        maxY: null,
         maximized: true,
+        element: null,
         position:{
           left: 'calc(50% - 800px / 2)',
           top: 'calc(50% - 500px / 2)',
@@ -39,8 +70,8 @@ import {resizeElement, set_resizable} from './plugins/resize.js'
       }
     },
     mounted(){
-      dragElement(this.$refs.window, this.$refs.header);
-      resizeElement(this.$refs.window);
+      this.dragElement(this.$refs.window, this.$refs.header);
+      this.resizeElement(this.$refs.window);
       this.handleMinimize();
       this.handleMaximize()
     },
@@ -60,8 +91,8 @@ import {resizeElement, set_resizable} from './plugins/resize.js'
         window.style.top = 0
         window.style.width = '100%'
         window.style.height = '100%'
-        set_draggable(false)
-        set_resizable(false)
+        this.set_draggable(false)
+        this.set_resizable(false)
       },
       handleMinimize(){
         this.maximized = false
@@ -70,11 +101,297 @@ import {resizeElement, set_resizable} from './plugins/resize.js'
         window.style.top = this.position.top
         window.style.width = this.position.width
         window.style.height = this.position.height
-        set_draggable(true)
-        set_resizable(true)
-      }
+        this.set_draggable(true)
+        this.set_resizable(true)
+      },
+      set_draggable(value){
+        this.draggable = value
+        if(this.draggable)
+        {
+          this.drag_handle.style.cursor = 'move'
+        }
+        else
+        {
+          this.drag_handle.style.cursor = 'default'
+        }
+    },
+    dragMouseDown(e) {
+      if(e.button !== 0) return
+      if(!this.draggable) return
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      this.pos3 = e.clientX;
+      this.pos4 = e.clientY;
+      document.onmouseup = this.closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = this.elementDrag;
+    },
+    elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      this.pos1 = this.pos3 - e.clientX;
+      this.pos2 = this.pos4 - e.clientY;
+      this.pos3 = e.clientX;
+      this.pos4 = e.clientY;
+      // set the element's new position:
+      this.element.style.top = (this.element.offsetTop - this.pos2) + "px";
+      this.element.style.left = (this.element.offsetLeft - this.pos1) + "px";
+    },
+    closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    },
+    dragElement(element, header = null) {
+      this.drag_handle = header || element;
+      this.element = element
+      this.drag_handle.onmousedown = this.dragMouseDown;
+      this.drag_handle.style.cursor = 'move';
+    },
+    set_resizable(value)
+    {
+        this.resizable = value
+        if(this.resizable)
+        {
+            this.corner1.style.cursor = 'nw-resize'
+            this.corner2.style.cursor = 'ne-resize'
+            this.corner3.style.cursor = 'sw-resize'
+            this.corner4.style.cursor = 'se-resize'
+            this.top.style.cursor = 'n-resize'
+            this.left.style.cursor = 'w-resize'
+            this.right.style.cursor = 'e-resize'
+            this.bottom.style.cursor = 's-resize'
+        }
+        else
+        {
+            this.corner1.style.cursor = 'default'
+            this.corner2.style.cursor = 'default'
+            this.corner3.style.cursor = 'default'
+            this.corner4.style.cursor = 'default'
+            this.top.style.cursor = 'default'
+            this.left.style.cursor = 'default'
+            this.right.style.cursor = 'default'
+            this.bottom.style.cursor = 'default'
+        }
+    },
+    resizeElement(element, minW = 100, minH = 100, size = 20)
+    {
+        this.element = element
+        this.top = document.createElement('div');
+        this.top.style.width = '100%';
+        this.top.style.height = size + 'px';
+        this.top.style.backgroundColor = 'transparent';
+        this.top.style.position = 'absolute';
+        this.top.style.top = - (size/2) + 'px';
+        this.top.style.left = '0px';
+        this.top.style.cursor = 'n-resize';
+
+        this.top.addEventListener('mousedown',this.resizeYNegative)
+
+        element.appendChild(this.top);
+
+        this.bottom = document.createElement('div');
+        this.bottom.style.width = '100%';
+        this.bottom.style.height = size + 'px';
+        this.bottom.style.backgroundColor = 'transparent';
+        this.bottom.style.position = 'absolute';
+        this.bottom.style.bottom = - (size/2) + 'px';
+        this.bottom.style.left = '0px';
+        this.bottom.style.cursor = 'n-resize';
+
+        this.bottom.addEventListener('mousedown',this.resizeYPositive)
+
+        element.appendChild(this.bottom);
+
+        this.left = document.createElement('div');
+        this.left.style.width = size + 'px';
+        this.left.style.height = '100%';
+        this.left.style.backgroundColor = 'transparent';
+        this.left.style.position = 'absolute';
+        this.left.style.top = '0px';
+        this.left.style.left = - (size/2) + 'px';
+        this.left.style.cursor = 'e-resize';
+
+        this.left.addEventListener('mousedown',this.resizeXNegative)
+
+        element.appendChild(this.left);
+
+        this.right = document.createElement('div');
+        this.right.style.width = size + 'px';
+        this.right.style.height = '100%';
+        this.right.style.backgroundColor = 'transparent';
+        this.right.style.position = 'absolute';
+        this.right.style.top = '0px';
+        this.right.style.right = - (size/2) + 'px';
+        this.right.style.cursor = 'e-resize';
+
+        this.right.addEventListener('mousedown',this.resizeXPositive)
+
+        element.appendChild(this.right);
+
+
+        this.corner1 = document.createElement('div');
+        this.corner1.style.width = size + 'px';
+        this.corner1.style.height = size + 'px';
+        this.corner1.style.backgroundColor = 'transparent';
+        this.corner1.style.position = 'absolute';
+        this.corner1.style.top = - (size/2) + 'px';
+        this.corner1.style.left = - (size/2) + 'px';
+        this.corner1.style.cursor = 'nw-resize';
+
+        this.corner1.addEventListener('mousedown',this.resizeXNegative)
+        this.corner1.addEventListener('mousedown',this.resizeYNegative)
+        
+        element.appendChild(this.corner1);
+
+        this.corner2 = document.createElement('div');
+        this.corner2.style.width = size + 'px';
+        this.corner2.style.height = size + 'px';
+        this.corner2.style.backgroundColor = 'transparent';
+        this.corner2.style.position = 'absolute';
+        this.corner2.style.top = - (size/2) + 'px';
+        this.corner2.style.right = - (size/2) + 'px';
+        this.corner2.style.cursor = 'ne-resize';
+
+        this.corner2.addEventListener('mousedown',this.resizeXPositive)
+        this.corner2.addEventListener('mousedown',this.resizeYNegative)
+
+        element.appendChild(this.corner2);
+
+        this.corner3 = document.createElement('div');
+        this.corner3.style.width = size + 'px';
+        this.corner3.style.height = size + 'px';
+        this.corner3.style.backgroundColor = 'transparent';
+        this.corner3.style.position = 'absolute';
+        this.corner3.style.bottom = - (size/2) + 'px';
+        this.corner3.style.left = - (size/2) + 'px';
+        this.corner3.style.cursor = 'sw-resize';
+
+        this.corner3.addEventListener('mousedown',this.resizeXNegative)
+        this.corner3.addEventListener('mousedown',this.resizeYPositive)
+
+        element.appendChild(this.corner3);
+
+        this.corner4 = document.createElement('div');
+        this.corner4.style.width = size + 'px';
+        this.corner4.style.height = size + 'px';
+        this.corner4.style.backgroundColor = 'transparent';
+        this.corner4.style.position = 'absolute';
+        this.corner4.style.bottom = - (size/2) + 'px';
+        this.corner4.style.right = - (size/2) + 'px';
+        this.corner4.style.cursor = 'se-resize';
+
+        this.corner4.addEventListener('mousedown',this.resizeXPositive)
+        this.corner4.addEventListener('mousedown',this.resizeYPositive)
+
+        element.appendChild(this.corner4);
+    },
+    get_int_style(key)
+    {
+        return parseInt(window.getComputedStyle(this.element).getPropertyValue(key));
+    },
+    resizeXPositive(e) {
+      if(e.button !== 0) return
+      if(!this.resizable) return;
+      e = e || window.event;
+      e.preventDefault();
+      const {clientX} = e;
+      this.offsetX = clientX - this.element.offsetLeft - this.get_int_style('width');
+      document.addEventListener('mouseup', this.resizeXPositiveCloseDragElement)
+      document.addEventListener('mousemove', this.resizeXPositiveElementDrag)
+    },
+    resizeXPositiveElementDrag(e) {
+        const {clientX} = e;
+        let x = clientX - this.element.offsetLeft - this.offsetX
+        if(x < this.minW) x = this.minW;
+        this.element.style.width =  x + 'px';
+    },
+    resizeXPositiveCloseDragElement() {
+      document.removeEventListener("mouseup", this.resizeXPositiveCloseDragElement);  
+      document.removeEventListener("mousemove", this.resizeXPositiveElementDrag);
+    },
+    resizeYPositive(e) 
+    {
+      if(e.button !== 0) return
+      if(!this.resizable) return;
+      e = e || window.event;
+      e.preventDefault();
+      const {clientY} = e;
+      this.offsetY = clientY - this.element.offsetTop - this.get_int_style('height');
+
+      document.addEventListener('mouseup',this.resizeYPositiveCloseDragElement)
+      document.addEventListener('mousemove',this.resizeYPositiveElementDrag)
+    },
+    resizeYPositiveCloseDragElement() 
+    {
+      document.removeEventListener("mouseup", this.resizeYPositiveCloseDragElement);  
+      document.removeEventListener("mousemove", this.resizeYPositiveElementDrag);
+    },
+    resizeYPositiveElementDrag(e) 
+    {
+        const {clientY} = e;
+        let y =  clientY - this.element.offsetTop - this.offsetY;
+        if(y < this.minH) y = this.minH;
+        this.element.style.height = y + 'px';
+    },
+    resizeXNegative(e) {
+      if(e.button !== 0) return
+      if(!this.resizable) return;
+      e = e || window.event;
+      e.preventDefault();
+      const {clientX} = e;
+      this.startX = this.get_int_style('left')
+      this.startW = this.get_int_style('width')
+      this.offsetX = clientX - this.startX;
+      this.maxX = this.startX + this.startW - this.minW
+
+      document.addEventListener('mouseup',this.resizeXNegativeCloseDragElement)
+      document.addEventListener('mousemove',this.resizeXNegativeElementDrag)
+    },
+    resizeXNegativeElementDrag(e) {
+          const {clientX} = e;
+          let x = clientX - this.offsetX
+          let w = this.startW + this.startX - x
+          if(w < this.minW) w = this.minW;
+          if(x > this.maxX) x = this.maxX;
+          this.element.style.left = x + 'px';
+          this.element.style.width = w + 'px';
+    },
+    resizeXNegativeCloseDragElement() {
+      document.removeEventListener("mouseup", this.resizeXNegativeCloseDragElement);  
+      document.removeEventListener("mousemove", this.resizeXNegativeElementDrag);
+    },
+    resizeYNegative(e) {
+      if(e.button !== 0) return
+      if(!this.resizable) return;
+      e = e || window.event;
+      e.preventDefault();
+      const {clientY} = e;
+      this.startY = this.get_int_style('top')
+      this.startH = this.get_int_style('height')
+      this.offsetY = clientY - this.startY;
+      this.maxY = this.startY + this.startH - this.minH 
+
+      document.addEventListener('mouseup',this.resizeYNegativeCloseDragElement,false)
+      document.addEventListener('mousemove',this.resizeYNegativeElementDrag,false)
+    },
+    resizeYNegativeElementDrag(e) {
+        const {clientY} = e;
+        let y =  clientY - this.offsetY
+        let h = this.startH + this.startY - y
+        if(h < this.minH) h = this.minH;
+        if(y > this.maxY) y = this.maxY;
+        this.element.style.top = y + 'px';
+        this.element.style.height = h + 'px';
+    },
+    resizeYNegativeCloseDragElement() {
+      document.removeEventListener("mouseup", this.resizeYNegativeCloseDragElement);  
+      document.removeEventListener("mousemove", this.resizeYNegativeElementDrag);
     }
-  }
+  },
+}
 </script>
 <style scoped>
 .window {
@@ -95,7 +412,7 @@ import {resizeElement, set_resizable} from './plugins/resize.js'
   margin-top: 0px;
   overflow: auto;
   width: 100%;
-  height: calc(100% - 45px); 
+  height: calc(100% - 30px); 
   padding: 10px;
 }
 .header > .title{
@@ -112,6 +429,7 @@ import {resizeElement, set_resizable} from './plugins/resize.js'
   border:1px solid var(--medium);
   color: #fff;
   height: 30px;
+
   
 }
 .nav-button{
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,68 @@
 <template>
-<div v-for="window in windows" :key="window">
-
-<window 
-:title="window.title"
-:icon="window.icon" 
-v-if="window.active"
-v-show="window.visible"
-@minus="window.visible = false;window.focus = false"
-@close="window.active = false"
-@focus="handleFocus(window)"
-:style="`z-index: ${window.zindex};`"
-><component  :is="window.component"></component></window>
-</div>
+  <div v-for="window in windows" :key="window">
+    <window 
+    :title="window.title"
+    :icon="window.icon" 
+    v-if="window.active"
+    v-show="window.visible"
+    @minus="window.visible = false;window.focus = false;autoFocus();"
+    @close="handleClose(window)"
+    @focus="handleFocus(window)"
+    :style="`z-index: ${window.zindex};`"
+    ><component  :is="window.component"></component></window>
+  </div>
   <div class="bar">
     <div 
-      v-for="window in windows" 
-      :key="window"
+      v-for="(window, index) in windows" 
+      :key="index"
       class="bar-item"
-      @click="()=>{
-        if(!window.active)
-        {
-          window.active = true
-          window.focus = true
-        }
-        window.visible = true
-        handleFocus(window)
-      }"
+      @click="handleClick(window)"
+      @contextmenu="event=>{
+          event.preventDefault();
+          event.stopPropagation();
+          window.menu = true;
+          // next tick
+          $nextTick(()=>{
+           this.$refs.dropdown[0].focus();
+          });
+          
+        }"
     > 
+    <div 
+      class="dropdown" 
+      :style="`--height:${(window.active?options_active:options).length*25}px;--width:200px;}`"
+      v-if="window.menu" 
+      @blur="window.menu = false"
+      ref="dropdown"
+      @click="event=>
+        {
+          event.preventDefault();
+          event.stopPropagation();
+        }"
+      tabindex="0" 
+      >
+      <p 
+        v-for="{name, fun, icon} in window.active?options_active:options" 
+        :key="name" 
+        @click="window.menu = false;fun(window);"
+      >
+      <img v-if="icon == '{icon}'" :src="require('@/assets/' + window.icon + '')" class="icon" alt="">
+      <i v-if="icon!=undefined && icon!='{icon}'" :class="`fa fa-${icon}`"></i> {{ name.replace(/{title}/g, window.title) }}
+      </p>
+    </div>
+
       <img :src="require('@/assets/' + window.icon + '')" class="icon" alt="">
-      <div class="bar-underline" :class="{'active':window.active, 'selected':window.focus
-      }"></div>
+      <div class="bar-underline" :class="{'active':window.active, 'selected':window.focus}"></div>
+    </div>
+    <div class="minimize-button"
+      @click="()=>{
+        windows.forEach(window=>{
+          window.visible = false;
+          window.focus = false
+        });
+        }"
+    >
+
     </div>
   </div>
 </template>
@@ -41,34 +74,54 @@ export default defineComponent({
   components: {
     window,
   },
-  created(){
-    for(const i in this.windows)
-    {
-      this.windows[i].zindex = i;
-    }
-  },
+
   data(){
     return {
+      options:[
+        {
+          icon:"{icon}",
+          name:"{title}",
+          fun: this.handleOpen
+        }
+      ],
+      options_active:[
+        {
+          icon:"{icon}",
+          name:"{title}",
+          fun: this.handleOpen
+        },
+        {
+          icon:"times",
+          name:"Close window",
+          fun: this.handleClose
+        }
+      ],
       windows: [
         {
           title:'Config',
           icon:'gear.png',
-          visible: false,
-          active: false,
-          focus:false,
           component:defineAsyncComponent(() => import('@/views/Config.vue'))
         },
         {
           title:'Solicitation',
           icon:'airplane.png',
-          visible: false,
-          active: false,
-          focus:false,
           component:defineAsyncComponent(() => import('@/views/Solicitation.vue'))
         }
       ],
     }
   },
+
+  created(){
+    for(const i in this.windows)
+    {
+      this.windows[i].zindex = i;
+      this.windows[i].active = false;
+      this.windows[i].visible = false;
+      this.windows[i].focus = false;
+      this.windows[i].menu = false;
+    }
+  },
+
   methods:{
     handleFocus(window){
       for(const i in this.windows)
@@ -89,7 +142,94 @@ export default defineComponent({
       this.windows.sort((a, b) => {
         return a.sort - b.sort
       })
+    },
+    
+    autoFocus()
+    {
+      var focus = false
+      var maxZindex = 0
+      var maxZindexWindow = null
+      for(const i in this.windows)
+      {
+        if(this.windows[i].visible)
+        {
+          if(this.windows[i].focus)
+          {
+            focus = true;
+          }
+          if(this.windows[i].zindex >= maxZindex)
+          {
+            maxZindex = this.windows[i].zindex
+            maxZindexWindow = this.windows[i]
+          }
+        }
+      }
+      if(!focus && maxZindexWindow)
+      {
+        maxZindexWindow.focus = true
+      }
+    },
+
+    clearFocus()
+    {
+      for(const i in this.windows)
+      {
+        this.windows[i].focus = false;
+      }
+    },
+
+    handleClick(window)
+    {
+        if(!window.active)
+        {
+          this.clearFocus()
+          window.active = true
+          window.visible = true
+          window.focus = true
+        }
+        else
+        if(!window.focus)
+        {
+          this.clearFocus()
+          window.focus = true
+          window.visible = true
+        }
+        else
+        {
+          if(!window.visible)
+          {
+            this.clearFocus()
+            window.visible = true
+            window.focus = true
+          }
+          else
+          {
+            window.visible = false
+            window.focus = false
+            this.autoFocus()
+            return
+          }
+        }
+        this.handleFocus(window)
+    },
+
+    handleClose(window)
+    {
+      window.active = false;
+      window.visible = false;
+      window.focus = false;
+      this.autoFocus();
+    },
+
+    handleOpen(window)
+    {
+        this.clearFocus()
+        window.active = true
+        window.visible = true
+        window.focus = true
+        this.handleFocus(window)
     }
+
   }
 })
 </script>
@@ -111,6 +251,7 @@ export default defineComponent({
   border-top: 1px solid var(--bright);
   --bar-color:white;
 }
+
 .bar-item{
   height: 100%;
   width: var(--size);
@@ -121,15 +262,18 @@ export default defineComponent({
   margin-left: 3px;
   margin-right: 3px;
 }
+
 .bar-item:hover .bar-underline{
   width: 40px;
   margin-left: -5px;
 }
+
 .bar-underline.active{
   width: 40px;
   margin-left: -5px;
   border-color:var(--bar-color)
 }
+
 .bar-underline{
   border-bottom: 3px solid rgb(145, 145, 145);
   margin-top: 6px;
@@ -137,10 +281,62 @@ export default defineComponent({
   width: 5px;
   transition: width .2s ease-in-out, margin-left .2s ease-in-out, border-color .2s ease-in-out;
 }
+
 .bar-item img{
   height: 100%;
 }
+
 .selected{
   --bar-color:rgb(97, 130, 241);
+}
+
+.dropdown{
+  background-color:var(--medium);
+  border:1px solid var(--bright);
+  width:var(--width);
+  height:calc(20px + var(--height));
+  margin-left: calc(20px - var(--width)/2);
+  margin-top:calc(-30px - var(--height));
+  margin-bottom: 10px;
+  padding-top:10px;
+  cursor:default;
+  border-radius:5px;
+  text-align: left;
+}
+.dropdown:focus{
+  outline:none;
+}
+.dropdown p{
+  margin-bottom: 0px;
+  padding-left: 15px;
+  cursor: pointer;
+}
+.dropdown p i{
+  margin-right: 5px;
+}
+.dropdown p img{
+  margin-right: 2px;
+  margin-left: -1px;
+  margin-top:-2px;
+  width:12pt
+}
+
+.dropdown p:hover{
+  background-color: var(--bright);
+}
+
+.minimize-button{
+  border: 1px solid var(--bright);
+  height:calc(100% + 12px);
+  width:15px;
+  float:right;
+  margin-left:-20px;
+  margin-top:-6px;
+  margin-right: -5px;
+  cursor: pointer;
+}
+
+.minimize-button:hover{
+  background-color: var(--bright);
 }
 </style>
